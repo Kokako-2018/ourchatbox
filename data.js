7bb5a342a9ab21aa6aba34e0e5c7dc5b97b18011
Priya Hiremath <priyahrish18@gmail.com>
2018-03-30 11:39:22 +1300

    datafile

---
+++ data.js
@@ -0,0 +1,129 @@
+import { url } from "inspector";
+
+var data = [
+    
+{
+     id: 1,
+    name: 'Annette',
+    imgurl: ,
+    text: ''
+},
+{
+    id: 2,
+    name: 'Annika',
+    imgurl: ,
+    
+    text: ''
+},
+{
+    id: 3,
+    name: 'Anthony',
+    imgurl: ,
+    text: ''
+},
+{
+    id: 4,
+    name: 'Ben',
+    imgurl:,
+    text: ''
+},
+{
+    id: 5,
+    name: 'Dana',
+    imgurl:,
+    text:''
+},
+{
+    id: 6,
+    name: 'Engie',
+    imgurl:,
+    text: ''
+},
+{
+    id: 7,
+    name: 'Hameet',
+    imgurl:,
+    text: ''
+},
+{
+    id: 8,
+    name: 'Harrison',
+    imgurl:,
+    text: ''
+},
+{
+    id: 9,
+    name: 'Kelly',
+    imgurl:,
+    text: ''
+},
+{
+    id: 10,
+    name: 'Laura',
+    imgurl:,
+    text: '',
+},
+{
+    id: 11,
+    name: 'Maddy',
+    imgurl:,
+    text: ''
+},
+{
+    id: 12,
+    name: 'Mark',
+    imgurl:,
+    text: ''
+},
+{
+    id: 13,
+    name: 'Priya',
+    imgurl:,
+    text: ''
+},
+
+{
+    id: 14,
+    name: 'Rosie',
+    imgurl:,
+    text: ''
+},
+{
+    id: 15,
+    name: 'Ruth',
+    imgurl:,
+    text: ''
+},
+{
+    id: 16,
+    name: 'Ross',
+    imgurl:,
+    text: ''
+},
+{
+    id: 17,
+    name: 'Sam',
+    imgurl:,
+    text: ''
+},
+{
+    id: 18,
+    name: 'Steve',
+    imgurl:,
+    text: ''
+},{
+    id: 19,
+    name: 'Theta',
+    imgurl:,
+    text: ''
+},
+{
+    id: 20,
+    name: 'Zach',
+    imgurl:,
+    text: ''
+},
+
+
+
+]
